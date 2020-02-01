7b588620f272aeabeae6f6f28c865034a76339ef
Ayman <aymankhawaldeh8@gmail.com>
2020-02-01 21:57:21 +0200

    done

--- challenges/arrayBinarySearch/__tests__/array-binary-search.test.js
+++ challenges/arrayBinarySearch/__tests__/array-binary-search.test.js
@@ -1,12 +1,13 @@
+/* eslint-disable new-cap */
 'use strict';
 
 let bainary = require('../lib/array-binary-search')
 
 describe('return an index of an item',()=>{
-    it('return a specific index of the item',()=>{
-        expect(bainary.BinarySearch([1,2,3], 2)).toBe(1);
-    });
-    it('return a falsy value for the item which does not exist in the array',()=>{
-        expect(bainary.BinarySearch([1,2,3], 4)).toBe(-1);
-    });
+  it('return a specific index of the item',()=>{
+    expect(bainary.BinarySearch([1,2,3], 2)).toBe(1);
+  });
+  it('return a falsy value for the item which does not exist in the array',()=>{
+    expect(bainary.BinarySearch([1,2,3], 4)).toBe(-1);
+  });
 })
--- challenges/arrayBinarySearch/lib/array-binary-search.js
+++ challenges/arrayBinarySearch/lib/array-binary-search.js
@@ -5,8 +5,8 @@ let arrayBainarySearch = module.exports= {};
 arrayBainarySearch.BinarySearch = (arr, input)=>{
   for(let i=0; i<arr.length; i++){
     if (arr[i]===input){
-     return i;
-   
+      return i;
+
     }
   }
   return -1;
