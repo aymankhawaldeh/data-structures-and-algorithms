c54a41e4b3be11bc9a8021c699769a6e7b079a54
Ayman <aymankhawaldeh8@gmail.com>
2020-02-28 18:45:45 +0200

    done

---
+++ challenges/hashtable/__tests__/hashtable.test.js
@@ -0,0 +1,60 @@
+
+/* eslint-disable strict */
+'use strict';
+
+const HashMap = require('../hashtable.js');
+
+describe('Hash Table Module',()=>{
+  it('Adding a key/value to your hashtable results in the value being in the data structure',()=>{
+    let hashMap = new HashMap(1024);
+    hashMap.add('name','shakespeare');
+    hashMap.storage.forEach(item=>{
+      expect(item.head.data.key).toEqual('name');
+      expect(item.head.data.value).toEqual('shakespeare');
+    });
+
+  });
+  it('Retrieving based on a key returns the value stored',()=>{
+    let hashMap = new HashMap(1024);
+    hashMap.add('age',23);
+    hashMap.storage.forEach(item=>{
+      expect(item.head.data.key).toEqual('age');
+      expect(item.head.data.value).toEqual(23);
+    });
+  });
+  it('Successfully returns null for a key that does not exist in the hashtable',()=>{
+    let hashMap = new HashMap(1024);
+    hashMap.add('age',23);
+    expect(hashMap.contain('hoppies')).toBeNull();
+    expect(hashMap.contain('cool')).toBeNull();
+  });
+  it('Successfully handle a collision within the hashtable',()=>{
+    let hashMap = new HashMap(1024);
+    hashMap.add('listen','ahmad');
+    hashMap.add('silent','hi');
+    let hashedKey = hashMap.hash('listen');
+    expect(hashMap.storage[hashedKey].head.data.key).toBeDefined();
+    expect(hashMap.storage[hashedKey].head.data.value).toBeDefined();
+    expect(hashMap.storage[hashedKey].head.next.data.key).toBeDefined();
+    expect(hashMap.storage[hashedKey].head.next.data.value).toBeDefined();
+  });
+  it('Successfully retrieve a value from a bucket within the hashtable that has a collision',()=>{
+    let hashMap = new HashMap(1024);
+    hashMap.add('listen','ahmad');
+    hashMap.add('silent','hi');
+    let hashedKey = hashMap.hash('listen');
+    expect(hashMap.storage[hashedKey].head.data.key).toEqual('silent');
+    expect(hashMap.storage[hashedKey].head.data.value).toEqual('hi');
+    expect(hashMap.storage[hashedKey].head.next.data.key).toEqual('listen');
+    expect(hashMap.storage[hashedKey].head.next.data.value).toEqual('ahmad');
+  });
+  it('Successfully hash a key to an in-range value',()=>{
+    let hashMap = new HashMap(1024);
+    hashMap.add('listen','ahmad');
+    hashMap.add('silent','hi');
+    let hashedKey = hashMap.hash('listen');
+    expect(hashedKey).toEqual(157);
+    expect(typeof hashedKey).toBe('number');
+  });
+
+});
