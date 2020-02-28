025f2c7ddaac4bf682c4e986345bfefc610917e7
Ayman <aymankhawaldeh8@gmail.com>
2020-02-28 18:00:55 +0200

    done

---
+++ challenges/mergeSort/__tests__/merge-sort.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mergeSort = require('../merge-sort.js').default;
+
+describe('mergeSort', () => {
+  it('should return a sorted array with no changes', () => {
+    const arr = [1, 1, 2, 3, 4, 5, 99, 1000, 10021];
+    expect(mergeSort(arr)).toEqual(arr);
+  });
+  it('should return a backwards-sorted array in sorted order', () => {
+    const backwards = [12121, 1221, 99, 88, 77, 66, 44, 22, 1, 0.123];
+    expect(mergeSort(backwards)).toEqual(backwards.reverse());
+  });
+  it('should return an empty array argument unchanged', () => {
+    const empty = [];
+    expect(mergeSort(empty)).toEqual(empty);
+  });
+  it('should return a single-element array unchanged', () => {
+    const one = [1];
+    expect(mergeSort(one)).toEqual(one);
+  });
+});
